5a43afbb319f53e9679e9ef9c1f80ba4e5a3924e
Johan Jarvi <johan.jarvi@nib.com.au>
2024-08-13 15:23:45 +1000

    Create a shortcut way to exit insert mode
    Thanks Sam

--- init.lua
+++ init.lua
@@ -198,6 +198,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Insert mode keybinds
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode easier' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
